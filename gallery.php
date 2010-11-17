8bdf8973ddc8b71fb3870d8962930153813dd704
Morous <mity@morous.org>
2010-11-17 15:42:57 +0100

    Big update of the mCtrl web site.

--- gallery.php
+++ gallery.php
@@ -3,7 +3,30 @@ require "_page.inc";
 page_header("Gallery");
 ?>
 
-<p>Under construction</p>
+<h2>Button Control</h2>
+<img src="gallery/button.png" style="float:right">
+<p>Button control provides emulation of split buttons (<tt>BS_SPLITBUTTON</tt>, 
+<tt>BS_DEFSPLITBUTTON</tt>) and themed icon buttons (<tt>BS_ICON</tt>) on 
+systems where such functionality is not available natively.</p>
+
+<p>With mCtrl, you can use these buttons safely since Windows 2000.</p>
+
+<h2>HTML Control </h2>
+<img src="gallery/html.png" style="float:right">
+<p>Control embedding power of web browser into into your applications.</p>
+<p>The control is leightweight wrapper of Internet Explorer COM object
+and enables you to incorporate local or remote HTML content into your 
+programs.</p>
+
+<h2>MDI Tab Control</h2>
+<img src="gallery/mditab.png" style="float:right">
+<p>For applications able to open multiple document, Win32API traditionaly 
+offers <a href="">MDI (Multiple Document Interface).</a> However it is 
+user unfriendly and modern applications like all popular web browsers usally 
+use tabs for this purpose instead.</p>
+<p>That's exactly what this control is intended for: It's a replacement
+for MDI. It offers similar functionality, but it has tab-like user experience
+consistent with Windows GUI.</p>
 
 <?php
 page_footer();
--- index.php
+++ index.php
@@ -3,12 +3,30 @@ require "_page.inc";
 page_header("Home");
 ?>
 
-<!--<div id="teaser">-->
-    <p>New to mCtrl? Start your survey on <a href="about.php">About</a> and <a href="gallery.php">Gallery</a>.</p>
-<!--</div>-->
+<div class="attention">
+    New to mCtrl?<br>
+    Start your survey on <a href="about.php">About</a> and <a href="gallery.php">Gallery</a>.
+</div>
 
-<h2>Current State</h2>
-<p>Under construction</p>
+<h2>Current Status</h2>
+<ul>
+    <li>API of the library is not yet rock-stable and can change any time.
+    However bigger fundamental changes are unlikely.</li>
+    <li>The control portfolio is poor. More are planned. Especially
+    grid control, property list control, and advanced edit control.</li>
+</ul>
+
+<h2>Call for Help</h2>
+<p>Do you like the project? Do you think the progress of the library is slow?</p>
+
+<p>Currently the project is one man show. Feel free to 
+<a href="mailto:mity[AT]morous[DOT]org">contact me</a> and join. 
+There certainly is a lot of space for other developers, documentators or 
+testers.</p>
+
+<p>Last but not least some help about graphic would be also appreciated. The 
+project still lacks any kind of logo and sometimes some icon for purposes 
+of the controls might be needed.</p>
 
 <h2>Recent News</h2>
 <?php page_news(3, 30); ?>
