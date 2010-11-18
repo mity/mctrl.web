53726d00749d894bd96378bcd03636790c660ed0
Morous <mity@morous.org>
2010-11-18 21:59:34 +0100

    Make a screenshots in gallery.php links
    to original-sized images.

--- gallery.php
+++ gallery.php
@@ -4,7 +4,7 @@ page_header("Gallery");
 ?>
 
 <h2>Button Control</h2>
-<img src="gallery/button.png" style="float:right">
+<a style="float:right" href="gallery/button-orig.png"><img src="gallery/button.png"></a>
 <p>Button control provides emulation of split buttons (<tt>BS_SPLITBUTTON</tt>, 
 <tt>BS_DEFSPLITBUTTON</tt>) and themed icon buttons (<tt>BS_ICON</tt>) on 
 systems where such functionality is not available natively.</p>
@@ -12,14 +12,14 @@ systems where such functionality is not available natively.</p>
 <p>With mCtrl, you can use these buttons safely since Windows 2000.</p>
 
 <h2>HTML Control </h2>
-<img src="gallery/html.png" style="float:right">
+<a style="float:right" href="gallery/html-orig.png"><img src="gallery/html.png"></a>
 <p>Control embedding power of web browser into into your applications.</p>
 <p>The control is leightweight wrapper of Internet Explorer COM object
 and enables you to incorporate local or remote HTML content into your 
 programs.</p>
 
 <h2>MDI Tab Control</h2>
-<img src="gallery/mditab.png" style="float:right">
+<a style="float:right" href="gallery/mditab-orig.png"><img src="gallery/mditab.png" style="float:right"></a>
 <p>For applications able to open multiple document, Win32API traditionaly 
 offers <a href="">MDI (Multiple Document Interface).</a> However it is 
 user unfriendly and modern applications like all popular web browsers usally 
