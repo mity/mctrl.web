6ba452cef04c0e8e71fe7d8e7258ec92fb8b6eba
Morous <mity@morous.org>
2010-12-17 18:10:34 +0100

    doc.php uses _latest to generate link to manual.

--- doc.php
+++ doc.php
@@ -5,7 +5,10 @@ page_header("Documentation");
 
 <h2>Reference Manual</h2>
 <p>The reference manual can be seen online here: 
-<a href="doc-0.7.0/">mCtrl 0.7.0 Reference Manual</a></p>
+<?php 
+    $latest = file_get_contents("_latest");
+    echo "<a href=\"doc-$latest/\">mCtrl $latest Reference Manual</a></p>";
+?>
 
 <p>It can also be <a href="download.php">downloaded</a>.</p>
 
