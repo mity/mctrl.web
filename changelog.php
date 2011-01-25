eb6edac340bae06de08fe7035ba99a9f064fb35d
Morous <mity@morous.org>
2011-01-25 19:38:14 +0100

    Fix typos, enahnce wording.

--- changelog.php
+++ changelog.php
@@ -5,9 +5,9 @@ page_header("ChangeLog");
 
 <h2>0.8.1</h2>
 <ul>
-    <li><tt>MC_WC_GRID</tt>: Fixed painting with styles <tt>MC_GS_COLUMNHEADERCUSTOM</tt> and <tt>MC_GS_ROWHEADERCUSTOM</tt>.
-    <li><tt>MC_WC_GRID</tt>: New messages <tt>MC_GM_SETGEOMETRY</tt> and <tt>MC_GM_GETGEOMETRY</tt>.
-    <li><tt>MC_VALUE</tt>: New value type <tt>MC_VALUETYPE_ID_HICON</tt>.
+    <li><tt>MC_WC_GRID</tt>: Fixed painting with styles <tt>MC_GS_COLUMNHEADERCUSTOM</tt> and <tt>MC_GS_ROWHEADERCUSTOM</tt>.</li>
+    <li><tt>MC_WC_GRID</tt>: New messages <tt>MC_GM_SETGEOMETRY</tt> and <tt>MC_GM_GETGEOMETRY</tt>.</li>
+    <li><tt>MC_VALUE</tt>: New value type <tt>MC_VALUETYPE_ID_HICON</tt>.</li>
     <li>Some minor fixes.</li>
 </ul>
 
@@ -15,8 +15,8 @@ page_header("ChangeLog");
 <ul>
     <li>Relicensing the project. Library sources are now under <a href="http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html">GNU LGPL 2.1</a>, examples in public domain.</li>
     <li><tt>MC_WC_GRID</tt>: New grid control added.</li>
-    <li><tt>MC_TABLE</tt> type: A data model for the grid control.</li>
-    <li><tt>MC_VALUE</tt> and <tt>MC_VALUETYPE</tt> types: A way how to hold arbitrary data which can be used e.g. as the table cells.</li>
+    <li><tt>MC_TABLE</tt>: A data model for the grid control.</li>
+    <li><tt>MC_VALUE</tt> and <tt>MC_VALUETYPE</tt>: A way how to hold arbitrary data which can be used e.g. as the table cells.</li>
 </ul>
 
 <h2>0.7.1</h2>
@@ -27,13 +27,13 @@ page_header("ChangeLog");
 
 <h2>0.7.0</h2>
 <ul>
-    <li>Prebuilt packages cpntain import library for MSVC.</li>
+    <li>Prebuilt packages now contain import library for MSVC.</li>
     <li>Fixed assorted bugs.</li>
-    <li>Rename some public structures and other public identifiers  to make them more consistent with each other and with Win32API naming conventions.</li>
+    <li>Rename some public structures and other public identifiers to make them more consistent with each other and with Win32API naming conventions.</li>
     <li><tt>MC_WC_HTML</tt>: Initial URL can be specfied during control creation with <tt>CreateWindow[Ex]()</tt> or via dialog template.</li>
-    <li><tt>MC_WC_HTML</tt>: New style <tt>MC_HS_NOCONTEXTMENU</tt></li>
-    <li><tt>MC_WC_HTML</tt>: Added support for application links and related notification <tt>MC_HN_APPLINK</tt></li>
-    <li><tt>MC_WC_HTML</tt>: Added notification <tt>MC_HN_DOCUMENTCOMPLETE</tt></li>
+    <li><tt>MC_WC_HTML</tt>: New style <tt>MC_HS_NOCONTEXTMENU</tt>.</li>
+    <li><tt>MC_WC_HTML</tt>: Added support for application links and related notification <tt>MC_HN_APPLINK</tt>.</li>
+    <li><tt>MC_WC_HTML</tt>: Added notification <tt>MC_HN_DOCUMENTCOMPLETE</tt>.</li>
     </li>
 </ul>
 
