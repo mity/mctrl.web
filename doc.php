910a9396b5b5b4ce822c53d6cac5a520133f24b6
Morous <mity@morous.org>
2011-04-26 19:03:39 +0200

    Add links to older versions of reference manual.

--- doc.php
+++ doc.php
@@ -4,13 +4,19 @@ page_header("Documentation");
 ?>
 
 <h2>Reference Manual</h2>
-<p>The reference manual can be seen online here: 
-<?php 
-    $latest = file_get_contents("_latest");
-    echo "<a href=\"doc-$latest/\">mCtrl $latest Reference Manual</a></p>";
+<p>The reference manual can be seen online here:</p>
+<ul>
+<?php
+    $items = scandir(".", 1);
+    for($i = 0; $i < count($items); $i++) {
+        $s = $items[$i];
+        if(sscanf($s, "doc-%s", $version) == 1)
+            echo "\t<li><a href=\"$s/\">mCtrl $version reference manual</a></li>\n";
+    }
 ?>
+</ul>
 
-<p>It can also be <a href="download.php">downloaded</a> for offline reading.</p>
+<p>The reference manual can also be <a href="download.php">downloaded</a> for offline reading.</p>
 
 <h2>Examples</h2>
 <p>Within the source code package you can find several examples demonstrating 
