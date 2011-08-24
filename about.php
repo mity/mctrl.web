f84e456f1edeecff699a0aace75f8c1bc39e150a
Morous <mity@morous.org>
2011-08-24 09:03:53 +0200

    Be more specific about licensing and fix some typos.

--- about.php
+++ about.php
@@ -14,27 +14,40 @@ controls.</p>
 <h2>Basic Features</h2>
 <ul>
     <li><b>Supported systems:</b> Windows 2000 and newer (both 32bit and 
-    64bit).</li>
+            64bit).</li>
     <li><b>API:</b> Based on windows messages, in a manner similar to the API
-    of <tt>USER32.DLL</tt> and <tt>COMCTL32.DLL</tt>.
+            of <tt>USER32.DLL</tt> and <tt>COMCTL32.DLL</tt>.
     <li><b>Unicode:</b> Internally all strings are Unicode. The interface 
-    mimics the Win32API duality controled with the preprocessor macro
-    <tt>UNICODE</tt>.</li>
+            mimics the Win32API duality controled with the preprocessor macro
+            <tt>UNICODE</tt>.</li>
     <li><b>Consistency:</b> The controls keep consistent look and feel 
-    with the system, including support for XP theming aka
-    <a href="http://msdn.microsoft.com/en-us/library/bb773187%28v=vs.85%29.aspx">
-    visual styles</a>.</li>
-    <li><b>Locale neutral:</b> The controls have no build-in strings
-    presented to the user, so the library is independent on current locale
-    and fits into applications supporting any set of languages and locales.</li>
+            with the system, including support for XP theming aka
+            <a href="http://msdn.microsoft.com/en-us/library/bb773187%28v=vs.85%29.aspx">
+            visual styles</a>.</li>
+    <li><b>Locale neutral:</b> The controls have no built-in strings presented
+            to the user, so the library is independent on current locale and
+            fits into applications supporting any set of languages and locales.</li>
 </ul>
 
 <h2>License</h2>
-<p>Since release 0.8.0 mCtrl is covered by 
+<p>Since release 0.8.0 mCtrl is covered by
 <a href="http://www.gnu.org/licenses/lgpl-2.1.html">GNU Lesser General Public
 License version 2.1</a> or (at your option) newer version of the license.</p>
 
-<p>Older releases were covered by stricter
+<p>In brief, this generally means that:</p>
+<ul>
+    <li>Any program or library, even commercial covered with any proprietary
+            license, is allowed to link against the mCtrl's import libraries
+            and distribute <tt>MCTRL.DLL</tt> along with the program.</li>
+    <li>You can modify <tt>MCTRL.DLL</tt> (or its source code) and distribute
+            such modified <tt>MCTRL.DLL</tt> only if the modifications are
+            licensed also under the terms of the LGPL 2.1 (or any later
+            version); or under the terms of GPL 2 (or any later version).</li>
+</ul>
+
+<p>Source code of examples is in public domain.</p>
+
+<p>Releases older then the 0.8.0 were covered by stricter 
 <a href="http://www.gnu.org/licenses/gpl-2.0.html">GNU General Public License
 version 2.0</a> or (at your option) newer version of the license.</p>
 
