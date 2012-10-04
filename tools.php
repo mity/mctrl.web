a9fe4be6a0f288267aef3d26706781660edcbfb4
Morous <mity@morous.org>
2012-10-04 20:01:15 +0200

    [tools.php] Add new page.

---
+++ tools.php
@@ -0,0 +1,31 @@
+<?php
+require "_page.inc";
+page_header("Tools", "slimbox2");
+?>
+
+<div class="attention">
+These tools are helpful for development of mCtrl project. If you intend only use mCtrl, then you probably do not need them.
+</div>
+
+<h2>Theme Explorer</h2>
+<a class="gallery" rel="lightbox" href="tools/theme-explorer.png"><img src="tools/theme-explorer-thumb.png" alt="screenshot of the button control"></a>
+
+<p>This is a small tool to explore visual styles (aka themes) on Windows XP and
+later, as provided by <tt>UXTHEME.DLL</tt>.</p>
+
+<p>It has been developed as an auxiliery tool to be used during the development
+of the mCtrl project (http://mctrl.sourceforge.net) but possibly other UI
+developers can find it useful too.</p>
+
+<ul>
+    <li><span class=hinline>License:</span> <a href="http://www.gnu.org/licenses/gpl-2.0.html">
+    GNU General Public License version 2.0</a> or (at your option) any later
+    version</li>
+    <li><span class=hinline>System requirements:</span> MS Windows XP or newer
+    (because of <tt>UXTHEME.DLL</tt>) with visual styles enabled</li>
+    <li><span class=hinline>Download:</span> soon</li>
+</ul>
+
+<?php
+page_footer();
+?>
