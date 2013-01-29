fba311f0a340d86cd07efbcf99398a18b1f030d9
Morous <mity@morous.org>
2013-01-29 18:17:21 +0100

    tools.php: Partial rewrite.

--- tools.php
+++ tools.php
@@ -7,8 +7,10 @@ page_header("Tools", "slimbox2");
 These tools are helpful for development of mCtrl project. If you intend only use mCtrl, then you probably do not need them.
 </div>
 
-<h2>Theme Explorer</h2>
-<a class="gallery" rel="lightbox" href="tools/theme-explorer.png"><img src="tools/theme-explorer-thumb.png" alt="screenshot of the button control"></a>
+<h2 id="theme-explorer">Theme Explorer</h2>
+<a class="gallery" rel="lightbox" href="tools/theme-explorer.png">
+    <img src="tools/theme-explorer-thumb.png" alt="screenshot of the button control">
+</a>
 
 <p>This is a small tool to explore visual styles (aka themes) on Windows XP and
 later, as provided by <tt>UXTHEME.DLL</tt>.</p>
@@ -18,13 +20,14 @@ of the mCtrl project (http://mctrl.sourceforge.net) but possibly other UI
 developers can find it useful too.</p>
 
 <ul>
+    <li><span class=hinline>Audience:</span> Custom control developers</li>
+    <li><span class=hinline>Download:</span><a href="http://sourceforge.net/projects/mctrl/files/theme-explorer/">
+    http://sourceforge.net/projects/mctrl/files/theme-explorer</a></li>
+    <li><span class=hinline>System requirements:</span> MS Windows XP or newer
+    (because of <tt>UXTHEME.DLL</tt>) with visual styles enabled</li>
     <li><span class=hinline>License:</span> <a href="http://www.gnu.org/licenses/gpl-2.0.html">
     GNU General Public License version 2.0</a> or (at your option) any later
     version</li>
-    <li><span class=hinline>System requirements:</span> MS Windows XP or newer
-    (because of <tt>UXTHEME.DLL</tt>) with visual styles enabled</li>
-    <li><span class=hinline>Download:</span><a href="http://sourceforge.net/projects/mctrl/files/theme-explorer/">
-    http://sourceforge.net/projects/mctrl/files/theme-explorer</a></li>
 </ul>
 
 <?php
