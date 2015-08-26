f217315827097d4d36116ea022ab2c6b4b5c91db
Martin Mitas <mity@morous.org>
2015-08-26 22:06:43 +0200

    gallery.php: Small cleanup.

--- gallery.php
+++ gallery.php
@@ -4,9 +4,9 @@ page_header("Gallery", "slimbox2");
 ?>
 
 <div class="info">
-The screenshots below are typically taken from small example application
+<p>The screenshots below are typically taken from small example application
 distributed with the <tt>MCTRL.DLL</tt>. To try them live simply download
-the latest binary package from the <a href="download.php">download page</a>.
+the latest binary package from the <a href="download.php">download page</a>.</p>
 </div>
 
 <h2>Button Control</h2>
