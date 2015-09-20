4516fadecb87441f3cb17b966ffa479bde85f58b
Martin Mitas <mity@morous.org>
2015-09-20 20:50:08 +0200

    gallery.php: Update.

--- gallery.php
+++ gallery.php
@@ -10,8 +10,13 @@ the latest binary package from the <a href="download.php">download page</a>.
 </div>
 
 <h2>Button Control</h2>
-<a class="gallery" rel="lightbox" href="gallery/button.png">
-    <img src="gallery/button-thumb.png" alt="screenshot of the button control">
+<a class="gallery" rel="lightbox-button" href="gallery/button-icon.png"
+title="mCtrl button control on Windows XP fixing the BS_ICON style issue.">
+    <img src="gallery/button-icon-thumb.png" alt="screenshot of the button control">
+</a>
+<a class="gallery" rel="lightbox-button" href="gallery/button-split.png"
+title="mCtrl button control on Windows 2000 with BS_SPLITBUTTON in action.">
+    <img src="gallery/button-split-thumb.png" alt="screenshot of the button control">
 </a>
 
 <p>Windows XP came with support for visual styles. Unfortunately
@@ -19,9 +24,9 @@ the latest binary package from the <a href="download.php">download page</a>.
 (<tt>BS_ICON</tt>).</p>
 
 <p>It was not until Windows Vista and <tt>COMCTL32.DLL</tt> 6.0 until the
-split buttons (<tt>BS_DEFSPLITBUTTON</tt>) were supported. Of course a lot of
-applications implemented them on their own again and again, but that's
-quite tedious work.</p>
+split buttons (<tt>BS_SPLITBUTTON</tt>, <tt>BS_DEFSPLITBUTTON</tt>) were
+supported. Of course a lot of applications implemented them on their own again
+and again, but that's quite tedious work.</p>
 
 <p>mCtrl button control provides reusable split buttons everywhere where mCtrl
 is supported (i.e. since Windows 2000) and emulates the styled icon buttons
@@ -29,7 +34,8 @@ in Windows XP (if the application uses visual styles).</p>
 
 
 <h2>Chart Control</h2>
-<a class="gallery" rel="lightbox" href="gallery/chart.png">
+<a class="gallery" rel="lightbox" href="gallery/chart.png"
+title="Many mCtrl chart control instances presenting various chart types.">
     <img src="gallery/chart-thumb.png" alt="screenshot of the chart control">
 </a>
 
@@ -50,7 +56,7 @@ a stacked variant.</p>
 
 
 <h2>Expand Control</h2>
-<a class="gallery" rel="lightbox" href="gallery/expand.png">
+<a class="gallery" rel="lightbox" href="gallery/expand.png" title="mCtrl expand control.">
     <img src="gallery/expand-thumb.png" alt="screenshot of the expand control">
 </a>
 
@@ -61,31 +67,34 @@ transition, and (for well designed dialogs) to magically compute the expanded
 
 
 <h2>Grid Control</h2>
-<a class="gallery" rel="lightbox" href="gallery/grid.png">
+<a class="gallery" rel="lightbox" href="gallery/grid.png" title="mCtrl grid control.">
     <img src="gallery/grid-thumb.png" alt="screenshot of the grid control">
 </a>
 
 <p>Users can easily understand large quantity of data when the data is
 presented in the right way. In many cases, the grid may be the right way.</p>
 
-<p>It allows to present large amout of data, and with the <tt>MC_HVALUE</tt>
-polymorphic type, the data may be of very different kind.</p>
-
 
 <h2>HTML Control </h2>
-<a class="gallery" rel="lightbox" href="gallery/html.png">
+<a class="gallery" rel="lightbox" href="gallery/html.png" title="mCtrl HTML control.">
     <img src="gallery/html-thumb.png" alt="screenshot of the HTML control">
 </a>
 
 <p>Embed power of web browser into into your application.</p>
 
-<p>The control is lightweight wrapper of Internet Explorer COM object
-and allows you to incorporate local or remote HTML content into your
-programs easily through the <tt>SendMessage()</tt> as any other control.</p>
+<p>The control is lightweight wrapper of Internet Explorer COM object and
+allows you to incorporate local or remote HTML content into your programs
+easily through the <tt>SendMessage()</tt> as any other control.</p>
+
+<p>Cooperation between the application and HTML may be achieved by getting
+notifications when user clicks on a link, and by calling JavaScript functions
+from the application.</p>
 
 
 <h2>Image View Control</h2>
-<a class="gallery" rel="lightbox" href="gallery/imgview.png">
+<a class="gallery" rel="lightbox" href="gallery/imgview.png"
+title="mCtrl image view control showing images loaded in various formats and
+presented with various scaling options.">
     <img src="gallery/imgview-thumb.png" alt="screenshot of the image view control">
 </a>
 
@@ -98,7 +107,7 @@ the DLL/EXE.</p>
 
 
 <h2>MDI Tab Control</h2>
-<a class="gallery" rel="lightbox" href="gallery/mditab.png">
+<a class="gallery" rel="lightbox" href="gallery/mditab.png" title="mCtrl MDI tab control.">
     <img src="gallery/mditab-thumb.png" alt="screenshot of the MDI tab control">
 </a>
 
@@ -114,7 +123,8 @@ replacement for MDI.</p>
 
 
 <h2>Menubar</h2>
-<a class="gallery" rel="lightbox" href="gallery/menubar.png">
+<a class="gallery" rel="lightbox" href="gallery/menubar.png"
+title="mCtrl manubar control embedded in a standard rebar control.">
     <img src="gallery/menubar-thumb.png" alt="screenshot of the menubar control">
 </a>
 
@@ -126,7 +136,7 @@ space on his monitor, by e.g. placing a toolbar and menubar to a single row.</p>
 on top of client area.</p>
 
 <h2>Tree-List View Control</h2>
-<a class="gallery" rel="lightbox" href="gallery/treelist.png">
+<a class="gallery" rel="lightbox" href="gallery/treelist.png" title="mCtrl tree-list view control.">
     <img src="gallery/treelist-thumb.png" alt="screenshot of the tree-list view control">
 </a>
 
